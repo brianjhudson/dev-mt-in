e05554fa01e038aaa4cdf49e0c8cbfd3a2ce8e28
Brian Hudson <brianjasonhudson@gmail.com>
2016-09-12 15:48:49 -0500

    "Initial Commit; set up views and routes"

---
+++ js/app.js
@@ -0,0 +1,35 @@
+angular.module('internApp', ['ui.router'])
+.config( function( $urlRouterProvider, $stateProvider ) {
+  $urlRouterProvider.otherwise('/');
+  $stateProvider
+    .state('home', {
+        url: '/'
+      , templateUrl: '/views/home.html'
+    })
+
+    .state('landing', {
+        url: '/landing'
+      , templateUrl: '/views/landing.html'
+    })
+
+    .state('friends', {
+        url: '/friends'
+      , templateUrl: '/views/friends.html'
+    })
+
+    .state('friend-search', {
+        url: '/friend-search'
+      , templateUrl: '/views/friend-search.html'
+    })
+
+    .state('friend-profile', {
+        url: '/friend-profile'
+      , templateUrl: '/views/friend-profile.html'
+    })
+
+    .state('update', {
+        url: '/update'
+      , templateUrl: '/views/update.html'
+    });
+
+})
